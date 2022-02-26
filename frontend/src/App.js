0aeb8eefabffac467082b82b72a8d7bae336ad72
Gilles Faucherand <gilles.faucherand@gmail.com>
2022-02-26 23:58:38 +0100

    link to github

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,30 @@
-import { Container } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import { Container, Grid } from "@mui/material";
 import Map from "./components/Map";
 import MapHeader from "./components/MapHeader";
 import ProjectsSelector from "./components/ProjectsSelector";
-
 function App() {
   return (
     <Container>
-      <h2>EcoPart demo</h2>
+      <Grid container space={2}>
+        <Grid item xs={2}>
+          <h2>EcoPart demo </h2>
+        </Grid>
+        <Grid item xs={2}>
+          <p>
+            [ more details on{" "}
+            <a
+              href="https://github.com/gfoo/EcoPart-demo"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GitHubIcon />
+            </a>{" "}
+            ]
+          </p>
+        </Grid>
+      </Grid>
+
       <ProjectsSelector />
       <MapHeader />
       <Map />
